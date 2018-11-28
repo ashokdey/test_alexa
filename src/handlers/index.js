cbf7ae99c926470d6a74bd7c19647a61828c74dc
Ashok Dey <ashokdey100@gmail.com>
2018-11-28 16:21:16 +0530

    --removed: legacy api and handlers

--- src/handlers/index.js
+++
@@ -1,5 +0,0 @@
-const v1 = require('./v1Handlers-legacy');
-
-module.exports = {
-  v1,
-};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 const alexaRootRoute = require('express').Router();
-const v1 = require('./alexa-legacy');
 const v2 = require('./alexa-latest');
 
-alexaRootRoute.use(v1);
 alexaRootRoute.use(v2);
 
 module.exports = alexaRootRoute;
